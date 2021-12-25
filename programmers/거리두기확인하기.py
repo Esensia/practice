66961babddfcd81d7a7c1b7f782b8ab6edca6564
Esensia <41403898+Esensia@users.noreply.github.com>
2021-12-25 23:43:28 +0900

    Create 거리두기확인하기.py

---
+++ programmers/거리두기확인하기.py
@@ -0,0 +1,30 @@
+def solution(places):
+    answer = []
+    
+    for place in places :
+        ans = -1
+        for i in range(len(place)):
+            if ans != -1:
+                break
+            for j in range(len(place)):
+                if ans != -1:
+                    break
+                if place[i][j] == "P":
+                    if (i + 1 < 5 and place[i+1][j] == "P") or (j + 1 < 5 and place[i][j+1] == "P"):
+                        ans = 0
+                        break
+                    if (i + 2 < 5 and place[i+1][j] == "O" and place[i+2][j] =="P") or (j +2 < 5 and place[i][j+1] == "O" and place[i][j+2] == "P"):
+                        ans = 0
+                        break
+                    if i+1<5 and j+1<5 and place[i+1][j+1]=="P" and not(place[i+1][j]=="X" and place[i][j+1]=="X") :
+                        ans = 0
+                        break
+                    if i-1>=0 and j+1<5 and place[i-1][j+1]=="P" and not(place[i-1][j]=="X" and place[i][j+1]=="X") :
+                        ans = 0
+                        break
+        if ans == -1:
+            answer.append(1)
+        else :
+            answer.append(0)
+        
+    return answer
