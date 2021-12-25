9adf3ce06161ebb26fd45046d0ecf595b154bc06
Esensia <41403898+Esensia@users.noreply.github.com>
2021-12-25 23:51:07 +0900

    Create 로또의 최고 순위와 최저 순위.py

---
+++ programmers/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,21 @@
+def solution(lottos, win_nums):
+    answer = []
+    count =0
+    count_zero = 0
+    for lotto in lottos :
+        if lotto in win_nums :
+            count += 1
+        elif lotto == 0:
+            count_zero += 1
+
+    if count+count_zero >= 2 :
+        answer.append(7-(count+count_zero))
+    else :
+        answer.append(6)
+        
+    if count >= 2:
+        answer.append(7-count)
+    else :
+        answer.append(6)
+
+    return answer
