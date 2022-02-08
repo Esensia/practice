cf2aee0a9ea73b3ad4594c0d0b98737ff266a15f
MinkiHong <41403898+Essentia21@users.noreply.github.com>
2022-02-08 14:06:00 +0900

    Create 신고 결과 받기.py

---
+++ programmers/신고 결과 받기.py
@@ -0,0 +1,30 @@
+def solution(id_list, report, k):
+    answer = []
+    
+    s = set()
+    dic = {}
+    dic2 = {}
+    for id in id_list :
+        dic[id] = []
+        dic2[id] = 0
+    
+    for i in report :
+        s.add(i)
+
+    for re in s :
+        names = re.split(" ")
+        dep = names[0]
+        des = names[1]
+        
+        dic[dep].append(des)
+        dic2[des] = dic2[des]+1
+        
+    for id in id_list :
+        count = 0
+        new_list = dic[id]
+        for l in new_list :
+            if dic2[l] >= k :
+                count+=1
+        answer.append(count)
+                
+    return answer
